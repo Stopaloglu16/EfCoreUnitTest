c29467a73e02c59e837690fa6e4a741ff8ceedce
Sertac Topaloglu <sertac.topaloglu@hotmail.co.uk>
2022-06-24 21:29:31 +0100

    Adding aggregate

--- Src/Domain/Child.cs
+++ Src/Domain/Child.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain
+﻿namespace Domain
 {
     public class Child
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        
+
         public int ParentId { get; set; }
+
+        public Parent Parent { get; set; }
     }
 }
--- Src/Domain/Parent.cs
+++ Src/Domain/Parent.cs
@@ -2,7 +2,7 @@
 {
     public class Parent
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
 
         public ICollection<Child> Children { get; set; }
