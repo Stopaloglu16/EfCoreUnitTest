2ccf02fde1fff4fb1881e0be166ae1c9b4ce9d39
Sertac Topaloglu <sertac.topaloglu@hotmail.co.uk>
2022-06-24 20:53:48 +0100

    Add project files.

---
+++ Src/Domain/Parent.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class Parent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Child> Children { get; set; }
+
+    }
+}
